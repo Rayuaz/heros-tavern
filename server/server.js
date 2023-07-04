5a0685aa5ca57447c859efafd1d59a4a4439f4b2
Alan Fernandes <rayquazabr@gmail.com>
2023-07-03 22:31:45 -0300

    Made the server port an environment variable

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-const PORT = 8000;
+const PORT = dotenv.config().parsed.API_PORT;
 const app = express();
 
 app.use(express.json());
